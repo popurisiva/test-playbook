fd8ea55b773e95563afbaf7c11711a5ec9b4b449
Siva Popuri <popuri.siva@gmail.com>
2015-11-17 15:46:57 -0600

    added action plug in for clc_server

---
+++ action_plugins/clc_server.py
@@ -0,0 +1,55 @@
+__author__ = 'brianalbrecht'
+import pydevd
+pydevd.settrace('localhost', port=5001, stdoutToServer=True, stderrToServer=True)
+
+from ansible.utils.plugins import action_loader
+from ansible.runner.return_data import ReturnData
+import json
+
+class ActionModule(object):
+
+    BYPASS_HOST_LOOP = False
+    TRANSFERS_FILES = False
+
+    def __init__(self, runner):
+        self.runner = runner
+
+    def run(self, conn, tmp, module_name, module_args, inject, complex_args=None, **kwargs):
+    	print '---->Inside the action plugin for module : {0}<----'.format(module_name)
+        module_return = self.runner._execute_module(conn=conn,
+                                                    tmp=tmp,
+                                                    module_name=module_name,
+                                                    args=module_args,
+                                                    inject=inject,
+                                                    complex_args=complex_args,
+                                                    **kwargs)
+        # with open("response_payload.json") as fp:
+        #     result = json.load(fp)
+        # module_return = ReturnData(conn=conn, comm_ok=True, result=result)
+
+        for server in module_return.result['servers']:
+            server_name = server['name']
+
+            ansible_ssh_user, ansible_ssh_pass = self._find_credentials(server_name)
+
+            server_args = {
+                "name": server['name'],
+                "ansible_ssh_host": server['ipaddress'],
+                "ansible_ssh_user": ansible_ssh_user,
+                "ansible_ssh_pass": ansible_ssh_pass,
+                "groupname": complex_args['group']
+            }
+            add_host = action_loader.get("add_host", self.runner)
+            add_host.run(conn=conn,
+                         tmp=tmp,
+                         module_name="add_host",
+                         module_args=None,
+                         inject=inject,
+                         complex_args=server_args,
+                         **kwargs)
+
+        return module_return
+
+    def _find_credentials(self, server_name):
+        clc_credentials = action_loader.get("clc_credentials", self.runner)
+        return clc_credentials.find_credentials(server_name)
