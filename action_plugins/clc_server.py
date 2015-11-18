13383fad422147e02109cbb2deec248a9888ca23
Siva Popuri <popurisiva@users.noreply.github.com>
2015-11-18 08:47:48 -0600

    Update clc_server.py

--- action_plugins/clc_server.py
+++ action_plugins/clc_server.py
@@ -1,8 +1,6 @@
 __author__ = 'brianalbrecht'
 
 from ansible.utils.plugins import action_loader
-from ansible.runner.return_data import ReturnData
-import json
 import os
 
 class ActionModule(object):
@@ -15,7 +13,6 @@ class ActionModule(object):
 
     def run(self, conn, tmp, module_name, module_args, inject, complex_args=None, **kwargs):
     	print '---->Inside the action plugin for module : {0}<----'.format(module_name)
-    	print '---->Current Dir: {0}<----'.format(os.getcwd())
         module_return = self.runner._execute_module(conn=conn,
                                                     tmp=tmp,
                                                     module_name=module_name,
@@ -23,14 +20,17 @@ class ActionModule(object):
                                                     inject=inject,
                                                     complex_args=complex_args,
                                                     **kwargs)
-        # with open("response_payload.json") as fp:
-        #     result = json.load(fp)
-        # module_return = ReturnData(conn=conn, comm_ok=True, result=result)
 
         for server in module_return.result['servers']:
+            print '---> Returned server object : {0} <---'.format(str(server))
             server_name = server['name']
 
             ansible_ssh_user, ansible_ssh_pass = self._find_credentials(server_name)
+            server_ip = server['ipaddress']
+            data_center = server['locationId']
+
+            print '---> Server IP: {0}'.format(server_ip)
+            print '---> Server Location: {0}'.format(data_center)
 
             server_args = {
                 "name": server['name'],
@@ -48,7 +48,19 @@ class ActionModule(object):
                          complex_args=server_args,
                          **kwargs)
 
+            self._add_ssh_config(server_ip, data_center)
+
         return module_return
+
+    @staticmethod
+    def _add_ssh_config(host_ip, data_center):
+        fp = open(os.path.join(os.pardir, 'ssh_config'), 'a+')
+        #ssh_bastion_host = 'SBAST-{0}.service.consul'.format(data_center)
+        ssh_bastion_host = 'SBAST-UC1-WFTC.service.consul'
+        fp.write('Host {0}\n'.format(host_ip))
+        fp.write('ProxyCommand ssh -o StrictHostKeyChecking=no -q -A root@{0} nc %h %p\n\n'.format(
+            ssh_bastion_host))
+        fp.close()
 
     def _find_credentials(self, server_name):
         clc_credentials = action_loader.get("clc_credentials", self.runner)
